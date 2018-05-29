db07125fa8a0cc139a03adf6a36893f97d77cb39
msingh <msingh@artshub.com.au>
2018-05-30 06:25:03 +1000

    Node REQ RES done

--- Week 4/NodeHello/index.js
+++ Week 4/NodeHello/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.use('/',(req, res)=>{
-    res.send('Hello World!');
+// app.use('/',(req, res)=>{
+//     //set the status of response
+//     res.status(200)
+//     res.type('.html')
+//     res.write('<h1>Hello World!</h1>')
+//     res.write('<p>Good to see you.</p>')
+//     res.end();
+// });
+
+app.use('/', (req,res) => {
+    var query = req.query;
+    var name = query.name;
+
+    res.status(200);
+
+    if(name){
+        res.write('Hello ' + name + '! How is out there?');
+    }
+    else{
+        res.write('Welcome Guest!');
+    }
+
+    res.end();
 });
 
 app.listen(3000, () => {
